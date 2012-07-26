d0af69249cbd721a20a12b510f52e33086fbbd0e
Kent Fredric <kentfredric@gmail.com>
2012-07-27 00:39:03 +1200

    Basic proof of concept, works for me

--- lib/File/Tempdir/ForPackage.pm
+++ lib/File/Tempdir/ForPackage.pm
@@ -3,11 +3,207 @@ use warnings;
 
 package File::Tempdir::ForPackage;
 
-# ABSTRACT:
+# ABSTRACT: Easy temporary directories associated with classes/objects
 
-use Moose;
+use Moo;
+use Sub::Quote qw( quote_sub );
 
-__PACKAGE__->meta->make_immutable;
-no Moose;
+=head1 DESCRIPTION
+
+This is mostly an interface wrapper for File::Temp::tempdir, stealing code from File::Tempdir;
+
+=over 4
+
+=item * I constantly forget how File::Tempdir works
+
+=item * I often want a tempdir with the name of the package working with it enshrined in the path
+
+=item * I constantly forget the magic glue syntax to get a folder inside a System Tempdir with a custom prefix and a user defined length of random characters.
+
+=back
+
+And this is designed to solve this simply.
+
+use File::TempDir::ForPackage;
+
+  my $tempdir = File::TempDir::ForPackage->new( package => __PACKAGE__ , use_version => 1 );
+  my $dir = $tempdir->dir();
+
+do shit in `$dir`
+$dir on Linux will be something like /tmp/perl-Some-Package-maybewith-a-VERSION-AFG14561/
+so if it crashes and leaves a tempdir behind, you will know who left that tempdir behind and have a way of cleaning it up.
+
+When $tempdir is destroyed, $dir will be cleaned;
+
+Additionally:
+
+   $dir->run_once_in(sub{
+
+   });
+
+Is there for people who don't trust scope auto-cleansing and want to know when the dir is reaped.
+
+Additionally, this code can be run in a tight loop creating and destroying lots of similary named tempdirs without risk of conflict.
+
+    for my $i ( 0 .. 30  ) {
+	$dir->run_once_in(sub { 
+		system 'find $PWD';
+	});
+    }
+
+This emits something like:
+
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-PzH4BD
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-5h8nkG
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-UXKt4S
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-Lqg2aW
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-DkNeq6
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-jRI_zF
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-j0_Gt1
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-iX1ddT
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-ZmvikK
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-QNGOUF
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-6wssvL
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-ZmwZxl
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-wIzRTs
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-xetCym
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-8Y0vyX
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-Zlqt6X
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-U5Z_Sa
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-sKmow1
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-rUND95
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-XjPSGF
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-ec8sZZ
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-_4NBwX
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-xM9i6l
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-p3FhJf
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-Zv0sso
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-rP8cAi
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303496-408662-iade0x
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303497-408662-fsDDPy
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303497-408662-FeCcfZ
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303497-408662-ta5yfg
+	/tmp/perl-File-Tempdir-ForPackage-versionundef-1343303497-408662-rdcQhF
+
+
+Except of course, with a package of your choosing, and possibly that packages version.
+
+
+=cut
+
+has package => (
+  is      => 'ro',
+  default => quote_sub q{
+	my $pkg = [ caller() ]->[0];
+	$pkg;
+  }
+);
+
+has with_version   => ( is => 'ro', default => quote_sub q{ undef } );
+has with_timestamp => ( is => 'ro', default => quote_sub q{ undef } );
+has with_pid       => ( is => 'ro', default => quote_sub q{ undef } );
+has num_random     => (
+  is  => 'ro',
+  isa => (
+    quote_sub q{
+		require File::Temp;
+		die "num_random ( $_[0] ) must be >= " . File::Temp::MINX()
+			if $_[0] < File::Temp::MINX();
+	}
+  ),
+  default => quote_sub q{ 8 },
+);
+has _preserve => ( is => 'rw', default => quote_sub q{ undef } );
+
+has _dir => ( is => 'lazy', clearer => 1, predicate => 1 );
+
+sub preserve {
+  my ( $self, @args ) = @_;
+  if ( not @args ) {
+    $self->_preserve(1);
+    return 1;
+  }
+  else {
+    if ( not $args[0] ) {
+      $self->_preserve(0);
+    }
+    else {
+      $self->_preserve(1);
+    }
+  }
+}
+
+sub _clean_pkg {
+  my ($package) = @_;
+  $package =~ s/::/-/g;
+  $package =~ s/[^A-Za-z0-9_-]+/_/g;
+  return $package;
+}
+
+sub _clean_ver {
+  my ($ver) = @_;
+  return 'versionundef' if not defined $ver;
+  $ver =~ s/[^v0-9_.]+/_/g;
+  return $ver;
+}
+
+sub _build__dir {
+  my ($self) = shift;
+  require File::Temp;
+
+  my $template = 'perl-';
+  $template .= _clean_pkg( $self->package );
+
+  if ( $self->with_version ) {
+    $template .= '-' . _clean_ver( $self->package->VERSION );
+  }
+  if ( $self->with_timestamp ) {
+    $template .= '-' . time;
+  }
+  if ( $self->with_pid ) {
+    $template .= '-' . $$;
+  }
+  $template .= '-' . ( 'X' x $self->num_random );
+
+  my $dir = File::Temp::tempdir( $template, TMPDIR => 1, );
+  return $dir;
+}
+
+sub dir {
+  my ($self) = shift;
+  return $self->_dir;
+}
+
+sub cleanse {
+  my ($self) = shift;
+  return $self unless $self->_has_dir;
+  if ( not $self->_preserve ) {
+    require File::Path;
+    File::Path::rmtree( $self->_dir, 0, 0 );
+  }
+  $self->_clear_dir;
+  return $self;
+}
+
+sub run_once_in {
+  my ( $self, $options, $code ) = @_;
+  $code = $options unless defined $code;
+  require File::pushd;
+  {
+    my $marker = File::pushd::pushd( $self->dir );
+    $code->( $self->dir );
+  }
+
+  # Dir POP.
+  $self->cleanse;
+  return $self;
+}
+
+sub DEMOLISH {
+  my ( $self, $in_g_d ) = @_;
+  $self->cleanse;
+}
+
+no Moo;
 
 1;
